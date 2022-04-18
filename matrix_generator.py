fdff65b8724c34821737bfd110bae01f85408b03
roen <1005623825@qq.com>
2022-04-18 14:25:25 +0800

    优化：使用coo_matrix和Lanczos特征值分解

--- matrix_generator.py
+++ matrix_generator.py
@@ -4,6 +4,8 @@ import argparse, os
 import time
 from configparser import ConfigParser
 from scipy.linalg import eigh
+from scipy.sparse import coo_matrix
+import scipy.sparse
 
 from scipy.io.wavfile import write
 
@@ -16,35 +18,6 @@ def getSignedTetVolume(ele_points):
     Dm = np.hstack((point0 - point3, point1 - point3, point2 - point3))
     volume = det(Dm) / 6
     return volume
-
-
-def getElementStiffness(ele_points, youngs, poisson):
-    lambda_ = youngs * poisson / (1 + poisson) / (1 - 2 * poisson)
-    mu_ = youngs * 0.5 / (1 + poisson) 
-    stiff = np.zeros((12, 12))
-    mb = np.zeros((4, 4))
-    for i in range(4):
-        mb[0][i] = ele_points[i][0]
-        mb[1][i] = ele_points[i][1]
-        mb[2][i] = ele_points[i][2]
-        mb[3][i] = 1
-    beta_ = np.linalg.inv(mb)
-    for i in range(4):
-        for j in range(4):
-            for a in range(3):
-                for b in range(3):
-                    I = i*3 + a
-                    J = j*3 + b
-                    stiff[I][J] = lambda_ * beta_[i][a] * beta_[j][b]
-                    # stiff[I][J] = lambda_ * beta_[i][a] * beta_[j][b] + mu_ * beta_[i][b] * beta_[j][a]
-                    if ( a == b ):
-                        sum = 0
-                        for k in range(3):
-                            sum += beta_[i][k] * beta_[j][k]
-                        stiff[I][J] += mu_ * sum
-                    stiff[I][J] *= 0.5 * abs(getSignedTetVolume(ele_points))
-    return stiff
-
 
 def getMeshInfo_vtk(filename):
     import meshio
@@ -95,7 +68,7 @@ class ModalAnalysis:
             if(ele % 50 == 0):
                 print("at element ", ele)
         self.M_ori = M_ori
-        self.M_fix = M_ori
+        self.M_coo = coo_matrix(M_ori)
         print('[ INFO] done')
 
     def constructK_ori(self):
@@ -140,13 +113,23 @@ class ModalAnalysis:
             if(ele % 50 == 0):
                 print("at element ", ele)
         self.K_ori = K_ori
-        self.K_fix = K_ori
+        self.K_coo = coo_matrix(K_ori)
         print('[ INFO] done')
+
+    def ged(self, k = 50):
+        from scipy.sparse.linalg import eigsh
+        self.evals, self.evecs = eigsh(A = self.K_coo, M = self.M_coo, which='LM', sigma=0, k=k)
 
     def saveMK_npz(self):
         print('[ INFO] saving Mass & Stiff matrix to' + self.output_path)
-        np.savez(self.output_path, mass=self.M_ori, stiff=self.K_ori)
+        # np.savez(os.path.join(self.output_path, 'dense_MK.npz'), mass=self.M_ori, stiff=self.K_ori)
+        scipy.sparse.save_npz(os.path.join(self.output_path, './M_coo.npz'), self.M_coo)
+        scipy.sparse.save_npz(os.path.join(self.output_path, './K_coo.npz'), self.K_coo)
         print('[ INFO] done')
+
+    def saveEigen(self):
+        print('[ INFO] saving Mass & Stiff matrix to' + self.output_path)
+        np.savez(os.path.join(self.output_path, 'eigen.npz'), evals=self.evals, evecs=self.evecs)
 
 # ./main.py -m 0 -ip './model/r02.vtk' -op './output/r02' 
 parser = argparse.ArgumentParser()
@@ -156,6 +139,7 @@ parser.add_argument('-op', '--outputpath', type=str, default='./test_eigen/T1.np
 
 FLAGS = parser.parse_args()
 
+# /home/roen/Projects/
 material_path = './material/material-{}.cfg'.format(FLAGS.material)
 
 ma_instance = ModalAnalysis()
@@ -169,5 +153,11 @@ ma_instance.constructK_ori()
 
 print("[ INFO] Saving MK...")
 ma_instance.saveMK_npz()
+
+print("[ INFO] GED...")
+ma_instance.ged()
+
+print("[ INFO] Saving evals evecs...")
+ma_instance.saveEigen()
 
 print("[ INFO] All completed.")
